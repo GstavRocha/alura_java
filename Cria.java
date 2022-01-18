1091300332c18d1b968af56cd9485b4e53f9d5d2
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-18 06:43:13 -0300

    Arquivo para teste de contas

---
+++ Cria.java
@@ -0,0 +1,39 @@
+public class Cria {
+    
+    public static void main(String[] args) {
+       Conta primeiraConta = new Conta();
+       primeiraConta.saldo = 200;
+       primeiraConta.titular ="Gustavo";
+
+       Conta segundaConta = new Conta();
+       segundaConta.saldo = 100;
+       segundaConta.titular = "Dany";
+
+
+       // System.out.println(primeiraConta.titular);
+       // System.out.println(primeiraConta.agencia);
+       // System.out.println(primeiraConta.saldo);
+       // System.out.println(segundaConta.titular);
+       // System.out.println(segundaConta.saldo);
+       // System.out.println(segundaConta.agencia);
+        
+       //TESTANDO REFERÊNCIA
+
+       Conta terceiraConta = primeiraConta;
+
+       System.out.println(terceiraConta);
+
+       System.out.println("Teste saldo " +terceiraConta.saldo);
+       System.out.println("Teste " +terceiraConta.titular);
+
+       terceiraConta.saldo += 300;
+       System.out.println("Teste saldo " +terceiraConta.saldo);
+       System.out.println("Teste saldo " +primeiraConta.saldo);
+
+       if(primeiraConta == terceiraConta){
+           System.out.println("Sim. são iguais!");
+       }else{
+           System.out.println("Não, apenas as referências");
+       }
+    }
+}
