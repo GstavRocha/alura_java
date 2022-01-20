a0969f6b3d493315b3f9f0de44e3bbabaf0f79e9
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-20 19:16:31 -0300

    Objeto dentro de referência

--- TestaMetodo.java
+++ TestaMetodo.java
@@ -1,12 +1,29 @@
+
 public class TestaMetodo {
+    /*
     public static void main(String[] args){
+
         Conta contaTeste = new Conta();
-        contaTeste.saldo = 100;
-        contaTeste.deposita(300);
+        contaTeste.saldo = 1000;
+        //contaTeste.deposita(300);
+
+        //System.out.println(contaTeste.saldo);
+        //boolean retirou = contaTeste.sacar(0);
+        
+        //System.out.println(contaTeste.saldo);
+        //System.out.println(retirou);
+
 
         System.out.println(contaTeste.saldo);
+        Conta danyConta = new Conta();
+        danyConta.saldo = 200;
 
+        contaTeste.transferencia(0,danyConta);
+        System.out.println("Saldo teste é "+contaTeste.saldo);
+        System.out.println("Saldo Dany é "+danyConta.saldo);
     } 
+
+*/
         
     
 }
